5b1d35e0bfe9192dcd5f474e80341ca0e4d176a9
Atharv123 <atharvmca2025_123@ncrdsims.edu.in>
2026-05-17 17:44:42 +0530

    added email debug

--- server.js
+++ server.js
@@ -525,7 +525,8 @@ app.post("/place-order", async (req,res)=>{
 
 /* SEND EMAIL IN BACKGROUND */
 try{
-
+    console.log("EMAIL GOING TO:", email);
+    
     await resend.emails.send({
 
         from:"onboarding@resend.dev",
